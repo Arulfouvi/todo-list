cda71d43fd88f3255c6cdc7e03f22d043bd84392
Arulfouvi <Arul#fouvi.com>
2024-10-01 16:46:43 +0530

    Feature: List tasks

---
+++ index.js
@@ -0,0 +1,6 @@
+// index.js
+const tasks = ["Buy groceries", "Clean the house", "Pay bills"];
+
+console.log("Welcome to the To-Do List App!");
+console.log("Your tasks:");
+tasks.forEach((task) => console.log("- " + task));
